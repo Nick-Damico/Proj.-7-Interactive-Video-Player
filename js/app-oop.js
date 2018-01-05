27c2ed3fbec4875509f12a35f47d0c8dd115b51d
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-04 23:20:47 -0500

    rebuilding app to model data and logic in an Object oriented design pattern

---
+++ js/app-oop.js
@@ -0,0 +1,9 @@
+// Author: Nicholas D'Amico
+// Contact: nickalan82@icloud.com
+// Checked project in: Chorme 54.0.02840.71, FireFox 50.0 & 49.0.1, safari 10.0.1
+// Treehouse Project 7: Interactive Video Player
+
+//Load Js when document is ready
+
+//Constructor Class JavaScript OOP solution
+//....

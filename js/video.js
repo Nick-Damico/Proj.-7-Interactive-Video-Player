b82480e5e96ee3a275d68f6000b02a1665ede029
Nick-Damico <nickalan82@icloud.com>
2024-08-22 14:56:56 -0400

    feat: setup video construction parameters
    These are all required. This needs to have some validations that raise
    console error messages if one of the video components is not found.

--- js/video.js
+++ js/video.js
@@ -1,2 +1,21 @@
 export default class Video {
+  constructor({
+    videoPlayerId,
+    videoContainerId,
+    videoControlsId,
+    progressBarId,
+    playButtonId,
+    volumeButtonId,
+    fullScreenButtonId
+  }) {
+    this._videoContainer = document.getElementById(videoContainerId)
+    this._videoPlayer = document.getElementById(videoPlayerId)
+    this._videoControls = document.getElementById(videoControlsId)
+    this._progressBar = document.getElementById(progressBarId)
+    this._playButton = document.getElementById(playButtonId)
+    this._volumeButton = document.getElementById(volumeButtonId)
+    this._fullScreenButton = document.getElementById(fullScreenButtonId)
+
+  }
+  }
 }

ead0e2cca1719ab5f3404b10d38f8ea6d0b04a37
Nick-Damico <nickalan82@icloud.com>
2024-08-22 14:19:17 -0400

    feat(wip): replace app.js with main.js
    First step in refactoring the old jQuery dependent code to newer ES6
    module style JavaScript code.

---
+++ main.js
@@ -0,0 +1,15 @@
+// Breaks existing scss
+// import './style.css'
+import Video from './js/video.js'
+
+console.log('main loaded')
+
+new Video({
+  videoPlayerId: 'videoPlayer',
+  videoContainerId: 'videoContainer',
+  videoControlsId: 'videoControls',
+  progressBarId: 'progressBar',
+  playButtonId: 'playBtn',
+  volumeButtonId: 'volumeBtn',
+  fullScreenButtonId: 'fullScreenBtn'
+})
